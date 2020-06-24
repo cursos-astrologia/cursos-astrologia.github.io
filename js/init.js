bd8c70856ce32d111fd6da7d79e522cce4cbdc01
Leonardo-Rocca <leonardo_7@live.com.ar>
2020-06-24 20:45:37 -0300

    cambio background

--- js/init.js
+++ js/init.js
@@ -3,13 +3,22 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+  
+    [
+    	{title:"Introducción ¿qué es la astrología?",duration:"1:05",items:[
+    	{description:"Historia- de donde viene",enabled:true,duration:"10:05"},
+    	{description:"Componentes de una carta",enabled:false,duration:"8:00"},
+    	]},
+    	{title:"Planetas",duration:"1:05",items:[
+    	{description:"Planetas introducción",duration:"3:05"},
+    	{description:"Signos, elementos y modos",duration:"3:45"},
+    	{description:"Casas y casas angulares",duration:"1:05"},
+    	]},
+    	{title:"Ascendente",duration:"6:05",items:[
+    	{description:"Ascendente introducción",duration:"10:05"},
+    	]},
 
-
-    [{title:"video de astro",duration:"1:05",items:[
-    	{description:"primera descripcion",enabled:true,duration:"10:05"},
-    	{description:"otra descripcion",enabled:false,duration:"8:00"},
-    	{description:"3ra descripcion",duration:"8:00"},
-    	]}].forEach((category,index)=>{
+    	].forEach((category,index)=>{
     	 $('#contenido-collapsable').append(` <li class="collection">
 			<div class="collapsible-header row-striped"><i class="material-icons">play_arrow</i> ${category.title}</div>
         		<div class="collapsible-body" id="video-section-${index}">
@@ -24,7 +33,7 @@
 					               ${video.description}
 					            </div>              
 					            <div style="${!video.enabled?'display:none':''}">
-					              <a href="#udemy"> Vista previa</a>
+					              <a href="#curso-progreso"> Vista previa</a>
 					            </div>    
 					            <div >${video.duration} </div>
           </div`);
@@ -37,3 +46,44 @@
    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+
+
+
+
+
+
+
+/*
+ <li class="active">
+        <div class="collapsible-header row-striped"><i class="material-icons">play_arrow</i>Video Introductorio al curso dignidades esenciales para astrologos </div>
+        <div class="collapsible-body">
+          <div class="flex flex-space-between">
+            <i class="material-icons ">play_circle_filled</i>
+            <div class="video-description">
+              En este video les cuento de que se trata el curso de dignidades esenciales, a quien esta dirigido y que habrán aprendido al finalizar el curso.
+            </div>              
+            <div >
+              <a href="#udemy"> Vista previa</a>
+            </div>    
+            <div >
+              1:05
+            </div>
+          </div>
+          <div class="divider"></div>            
+          <div class="row">
+            <div class="col s12 m8">
+              Introducción a dignidades esenciales, elementos y cualidades de los signos.
+            </div>              
+            <div class="col s12 m2">
+              <a href="#udemy"> Vista previa</a>
+            </div>    
+            <div class="col s12 m2">
+              2:05
+            </div>
+          </div>
+
+        </div>
+      </li>*/
