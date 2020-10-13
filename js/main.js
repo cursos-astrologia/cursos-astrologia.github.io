315e4d3e0842aab3f6827308a80d398496f21032
Leonardo-Rocca <leonardo_7@live.com.ar>
2020-10-12 23:44:27 -0300

    change cupon

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function main() {
-	 const DISCOUNT_PAGE = "https://www.udemy.com/course/astrologia-desde-cero/?couponCode=PROMSEP_2020"
-	 const GIFT_PAGE = "https://www.udemy.com/gift/astrologia-desde-cero/?couponCode=PROMSEP_2020"
+	 const DISCOUNT_PAGE = "https://www.udemy.com/course/astrologia-desde-cero/?couponCode=CURSOASTRODESC"
+	 const GIFT_PAGE = "https://www.udemy.com/gift/astrologia-desde-cero/?couponCode=CURSOASTRODESC"
  
 
     const addLink = (selector,link)=> $(selector).attr("href", link).attr("rel","noreferrer").attr("target","_blank");
