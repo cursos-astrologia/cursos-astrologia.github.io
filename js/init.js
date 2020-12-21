6e81ade64792a574b50edb9f87991aa33de566fa
Leonardo-Rocca <leonardo_7@live.com.ar>
2020-12-21 11:42:39 -0300

    hide nav

--- js/init.js
+++ js/init.js
@@ -197,6 +197,30 @@
     $("#prevCom").click(prevComment)
 
 
+
+
+
+
+
+ var c, currentScrollTop = 0,
+       navbar = $('nav');
+
+   $(window).scroll(function () {
+      var a = $(window).scrollTop();
+      var b = navbar.height();
+     
+      currentScrollTop = a;
+     
+      if (c < currentScrollTop && a > b + b) {
+        navbar.addClass("scrollUp");
+      } else if (c > currentScrollTop && !(a <= b)) {
+        navbar.removeClass("scrollUp");
+      }
+      c = currentScrollTop;
+  });
+  
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
